f0e0154bc3425fa49fb11e69695ff276ae696ab3
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-08-07 11:23:50 -0500

    Added mouse movement. Also renamed jump action.

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -9,6 +9,22 @@ public partial class Player : CharacterBody3D
     // Get the gravity from the project settings to be synced with RigidBody nodes.
     public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
 
+    bool mouseInput = false;
+    float rotationInput;
+    float tiltInput;
+    float pitch;
+    Vector3 mouseRotation;
+    [Export]
+    float tiltLowerLimit = Mathf.DegToRad(-90);
+    [Export]
+    float tiltUpperLimit = Mathf.DegToRad(90);
+    [Export]
+    Node3D cameraController;
+
+    public override void _Ready()
+    {
+        Input.MouseMode = Input.MouseModeEnum.Captured;
+    }
     public override void _PhysicsProcess(double delta)
     {
         Vector3 velocity = Velocity;
@@ -17,8 +33,10 @@ public partial class Player : CharacterBody3D
         if (!IsOnFloor())
             velocity.Y -= gravity * (float)delta;
 
+        UpdateCamera(delta);
+
         // Handle Jump.
-        if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+        if (Input.IsActionJustPressed("jump") && IsOnFloor())
             velocity.Y = JumpVelocity;
 
         Vector2 inputDir = Input.GetVector("moveLeft", "moveRight", "moveForward", "moveBackward");
@@ -37,7 +55,30 @@ public partial class Player : CharacterBody3D
         Velocity = velocity;
         MoveAndSlide();
     }
-    public override void _Process(double delta)
+    public override void _UnhandledInput(InputEvent @event)
     {
+        if (@event is InputEventMouseMotion mouseEvent && Input.MouseMode == Input.MouseModeEnum.Captured)
+        {
+            mouseInput = true;
+            rotationInput = -mouseEvent.Relative.X;
+            tiltInput = -mouseEvent.Relative.Y;
+        }
+    }
+    public void UpdateCamera(double delta)
+    {
+        mouseRotation.X += tiltInput * (float)delta;
+        mouseRotation.X = Mathf.Clamp(mouseRotation.X, tiltLowerLimit, tiltUpperLimit);
+        mouseRotation.Y += rotationInput * (float)delta;
+        pitch += tiltInput * (float)delta;
+        pitch = Mathf.Clamp(pitch, tiltLowerLimit, tiltUpperLimit);
+
+        // Rotate the player node around the Y-axis for horizontal camera movement
+        Rotate(Vector3.Up, rotationInput * (float)delta);
+
+        // Rotate the camera controller node around the X-axis for vertical camera movement
+        cameraController.Rotation = new Vector3(pitch, cameraController.Rotation.Y, cameraController.Rotation.Z);
+
+        rotationInput = 0.0f;
+        tiltInput = 0.0f;
     }
 }
