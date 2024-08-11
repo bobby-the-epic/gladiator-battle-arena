e2b1cb035f803c21c9500ea701dbba38df2e9ad4
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-08-11 11:40:57 -0500

    Removed resource loading.

--- Scripts/Main.cs
+++ Scripts/Main.cs
@@ -3,13 +3,9 @@ using System;
 
 public partial class Main : Node
 {
-    Node world;
-    Node level1 = ResourceLoader.Load<PackedScene>("res://level1.tscn").Instantiate();
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        // world = GetNode("World");
-        // world.AddChild(level1);
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
