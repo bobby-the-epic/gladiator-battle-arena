2aec9553d8ee839a2263cfef71cf475ce30bf4f7
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-09-01 19:38:49 -0500

    Changed main menu node to be a UI node.

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -3,18 +3,10 @@ using System;
 
 public partial class MainMenu : Node
 {
-    [Export]
-    Node3D cameraPivot;
-
-    [Export]
-    PackedScene gladiatorScene;
-
     public override void _Ready()
     {
     }
-
     public override void _Process(double delta)
     {
-        cameraPivot.Rotate(Vector3.Up, (float)delta * .25f);
     }
 }
