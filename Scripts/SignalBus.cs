9dc750860ddf96cd1c9212e5358fe30e16741c9e
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-09-01 19:31:59 -0500

    Added signal bus script.

---
+++ Scripts/SignalBus.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public partial class SignalBus : Node
+{
+    // Static property so that all nodes can access the signal bus without getting a reference.
+    public static SignalBus Instance { get; private set; }
+
+    [Signal]
+    public delegate void DamageTakenEventHandler(int health, float angle);
+    [Signal]
+    public delegate void GladiatorDiedEventHandler();
+    [Signal]
+    public delegate void GameStartEventHandler();
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
+}
