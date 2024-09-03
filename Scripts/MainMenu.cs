7b124df074a1ef2faee26f838ae50d85c4ec8d35
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-09-02 19:47:19 -0500

    Added basic options menu.

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -3,12 +3,20 @@ using System;
 
 public partial class MainMenu : Control
 {
+    [ExportGroup("Buttons")]
     [Export]
     Button playButton;
     [Export]
     Button optionsButton;
     [Export]
     Button quitButton;
+    [Export]
+    Button backButton;
+    [ExportGroup("Menu Screens")]
+    [Export]
+    Control titleScreen;
+    [Export]
+    Control optionsScreen;
     public override void _Ready()
     {
         // Emit signal when the player presses the play button.
@@ -16,6 +24,16 @@ public partial class MainMenu : Control
         {
             Hide();
             SignalBus.Instance.EmitSignal(SignalBus.SignalName.GameStart);
+        };
+        optionsButton.Pressed += () =>
+        {
+            optionsScreen.Show();
+            titleScreen.Hide();
+        };
+        backButton.Pressed += () =>
+        {
+            optionsScreen.Hide();
+            titleScreen.Show();
         };
         quitButton.Pressed += () => GetTree().Quit();
     }
