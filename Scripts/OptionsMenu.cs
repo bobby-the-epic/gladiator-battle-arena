5a837bad965bc8b0023bf831ea5d6f4bc9de5414
bobby-the-epic <174213631+bobby-the-epic@users.noreply.github.com>
2024-09-08 11:06:17 -0500

    Added mouse sensitivity option to options menu.

--- Scripts/OptionsMenu.cs
+++ Scripts/OptionsMenu.cs
@@ -5,13 +5,20 @@ public partial class OptionsMenu : Control
 {
     Godot.Collections.Array<Node> audioStreams;
     AudioStreamPlayer[] audioStreamPlayers;
+    Node player;
 
     [Export]
     HSlider volumeControl;
+    [Export]
+    HSlider sensitivityControl;
 
     public override void _Ready()
     {
+        player = GetTree().GetFirstNodeInGroup("player");
+        if (player != null)
+            sensitivityControl.Value = (double)player.Get("mouseSensitivity");
         volumeControl.Value = Main.volume;
+
         // Get the audio stream players in the audio group
         audioStreams = GetTree().GetNodesInGroup("audio");
         audioStreamPlayers = new AudioStreamPlayer[audioStreams.Count];
@@ -19,7 +26,9 @@ public partial class OptionsMenu : Control
         {
             audioStreamPlayers[counter] = (AudioStreamPlayer)audioStreams[counter];
         }
+
         volumeControl.ValueChanged += OnVolumeChanged;
+        sensitivityControl.ValueChanged += OnSensitivityChanged;
         for (int counter = 0; counter < audioStreams.Count; counter++)
             audioStreamPlayers[counter].VolumeDb = Main.volume;
     }
@@ -29,5 +38,10 @@ public partial class OptionsMenu : Control
         for (int counter = 0; counter < audioStreams.Count; counter++)
             audioStreamPlayers[counter].VolumeDb = (float)volume;
         Main.volume = (int)volume;
+    }
+    private void OnSensitivityChanged(double sensitivity)
+    {
+        if (player != null)
+            player.Set("mouseSensitivity", (float)sensitivity);
     }
 }
